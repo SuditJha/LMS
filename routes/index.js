fd86aaa8f6dc4cf1450db887fbbaa8afa01e4ea7
Sudit Kumar Jha <jsudit2@gmail.com>
2024-03-15 23:20:28 +0530

    Essential improvement #1 > Added Static Files > Added / route > Added /login route > Added /signup route > Linked the static files with respective routes > Bugs:- > Static JS files Not working > Forms .preventDefault not working > Suggesstion:- > Change Db to mongoDB > Add mongoose > Add Authentication - Install passport, passport-local, passport-local-mongoose, express-session

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index');
 });
+
+router.get('/login', function (req, res) {
+  res.render('login')
+})
+
+router.get('/signup', function (req, res) {
+  res.render('signup')
+})
 
 module.exports = router;
